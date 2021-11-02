c557a29313d0a184731a546ed04bacaa5fa74f88
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2021-11-01 20:24:24 -0400

    touch up 3

--- src/components/AddAmidites/AddAmidites.js
+++ src/components/AddAmidites/AddAmidites.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./AmiditesTable.css";
+import "./AddAmidites.css";
 import { db } from "../../firebaseConfig";
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
